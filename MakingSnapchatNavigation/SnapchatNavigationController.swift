5154b76b5af643eff24bd9bd61860c3fd1ca9e62
Veektah Hoodeh <veektahhoodeh@gmail.com>
2018-06-28 13:01:38 +0300

    SnapchatNavigationController added

---
+++ MakingSnapchatNavigation/SnapchatNavigationController.swift
@@ -0,0 +1,31 @@
+//
+//  SnapchatNavigationController.swift
+//  MakingSnapchatNavigation
+//
+//  Created by Victor Hyde on 28/06/2018.
+//  Copyright © 2018 Victor Hyde Code. All rights reserved.
+//
+
+import UIKit
+
+open class SnapchatNavigationController: UIViewController {
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Public interface
+
+    /// Sets view controllers.
+    public func setViewControllers(vcs: [UIViewController]) {
+
+    }
+
+    /// Sets background view.
+    public func setBackground(vc: UIViewController) {
+
+    }
+
+}
